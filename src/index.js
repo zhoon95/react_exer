a70f5d24601300229385a6607e219708dc508b2d
zhoon95 <zhoon95@naver.com>
2022-09-28 18:12:47 +0900

    chapter_03

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
-ReactDOM.reder(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
